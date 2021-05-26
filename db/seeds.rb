08312e94a32a430d84192059d0aa31fbfcfb8f66
florent astier <florenttier@gmail.com>
2021-05-26 14:42:06 +0200

    models done and begin controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE",  stars: 4}
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4}
+mc_do =  { name: "Mc DO", address: "Rue du McDo, Marseille", stars: 1}
+pierre =  { name: "resto chez pierre", address: "Rue du pierre, Marseille", stars: 3}
+bk =  { name: "bk", address: "place du bk, Marseille", stars: 0}
+puts "Finished!"
